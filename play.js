53404bbb4f3ae2ff029dfb9d161bf86bf716dd2d
donnahuang <donnaphzj@gmail.com>
2022-04-21 18:38:07 -0400

    connect and data

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require('net');
+const connect = () => {
+  const conn = net.createConnection({
+    host: '192.168.2.22',
+    port: 50541 
+  });
+  conn.setEncoding("utf8");
+  
+  conn.on('connect', () => {  
+    console.log('connected!');
+  })
+
+  conn.on('data', (data) => {
+    console.log('The server says: ', data);
+  })
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
